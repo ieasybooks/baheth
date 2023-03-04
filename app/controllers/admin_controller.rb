364f8006f0c317d0cf2a7c610744a89c4bafee05
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2023-03-04 18:05:30 +0000

    Add Admin controller

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,12 @@
+class AdminController < ApplicationController
+  before_action :authorized?
+
+  private
+
+  def authorized?
+    return if current_user.admin?
+
+    flash[:error] = 'You are not authorized to view that page.'
+    redirect_to root_path
+  end
+end
