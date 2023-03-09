658889a86eeb3d490c4282c3160a08ab88d0c027
Ali Hamdi Ali Fadel <AliOsm1997@gmail.com>
2023-03-09 21:12:25 +0000

    Remove hidden field from cue

--- app/views/admin/cues/_cue.json.jbuilder
+++ app/views/admin/cues/_cue.json.jbuilder
@@ -1,3 +1,3 @@
-json.extract! cue, :id, :content, :start_time, :end_time, :hidden,
+json.extract! cue, :id, :content, :start_time, :end_time,
               :medium_id, :playlist_id, :speaker_ids, :user_id, :created_at, :updated_at
 json.url admin_cue_url(cue, format: :json)
--- db/migrate/20230306055749_create_cues.rb
+++ db/migrate/20230306055749_create_cues.rb
@@ -4,7 +4,6 @@ class CreateCues < ActiveRecord::Migration[7.0]
       t.text :content
       t.interval :start_time
       t.interval :end_time
-      t.boolean :hidden, null: false, default: false
       t.references :medium, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
