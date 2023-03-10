331cb3581bec40efba5ae4a0fea66ef90179353b
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2023-03-10 19:09:42 +0000

    Refactor seconds_to_hh_mm_ss to support milliseconds and rename it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
-  def seconds_to_hh_mm_ss(seconds)
-    hours = (seconds / (60 * 60)) % 60
-    minutes = (seconds / 60) % 60
+  def seconds_to_time(seconds)
+    hours = (seconds / 3600).to_i
+    minutes = ((seconds / 60) % 60).to_i
+    seconds = (seconds % 60).to_f
 
-    format('%<hours>.2i:%<minutes>.2i:%<seconds>.2i', hours:, minutes:, seconds: seconds % 60)
+    format('%<hours>.2i:%<minutes>.2i:%<seconds>06.3f', hours:, minutes:, seconds:)
   end
 end
